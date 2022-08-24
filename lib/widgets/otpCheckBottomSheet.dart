0f0c8ff0958e4515ccb69f7d956decc03f07c267
Raj Sengupta <rajsengupta@Rohans-MacBook-Pro.local>
2022-08-24 15:46:09 +0530

    loading spinner after otp recieved

--- lib/widgets/otpCheckBottomSheet.dart
+++ lib/widgets/otpCheckBottomSheet.dart
@@ -25,9 +25,48 @@ class _OtpCheckState extends State<OtpCheck> {
         .verifyPhoneNumber(
       phoneNumber: '+91${widget._phone}',
       verificationCompleted: (PhoneAuthCredential credential) async {
-        userCredential =
-            await FirebaseAuth.instance.signInWithCredential(credential);
         _isLoading.value = true;
+        userCredential = await FirebaseAuth.instance
+            .signInWithCredential(credential)
+            .catchError((err) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                backgroundColor: Colors.white,
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Something Went wrong, Please try again later!',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      style: ButtonStyle(
+                        shadowColor: MaterialStatePropertyAll(Colors.amber),
+                        elevation: MaterialStatePropertyAll(8),
+                        shape: MaterialStatePropertyAll(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(26),
+                          ),
+                        ),
+                        backgroundColor: MaterialStatePropertyAll(Colors.amber),
+                      ),
+                      child: Text('Ok!'),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        });
         Navigator.of(context).pop();
         _isLoading.value = false;
       },
@@ -53,7 +92,7 @@ class _OtpCheckState extends State<OtpCheck> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 Text(
-                  'Could not verify phone no automatically , please type in recieved OTP',
+                  'Something Went wrong! If recieved OTP then type it in below.',
                   style: TextStyle(
                       color: Colors.black,
                       fontWeight: FontWeight.bold,
@@ -128,78 +167,81 @@ class _OtpCheckState extends State<OtpCheck> {
             SizedBox(
               height: 25,
             ),
-            _isLoading.value
-                ? SizedBox(
-                    child: Center(
-                        child: CircularProgressIndicator(
-                    color: Colors.amber,
-                  )))
-                : OtpTextField(
-                    textStyle: TextStyle(color: Colors.black),
-                    numberOfFields: 6,
-                    borderColor: Colors.amber,
-                    //set to true to show as box or false to show as dash
-                    showFieldAsBox: true,
-                    //runs when a code is typed in
-                    cursorColor: Colors.amber,
-                    enabledBorderColor: Colors.amber,
-                    focusedBorderColor: Colors.amber,
+            Obx(() {
+              return _isLoading.value
+                  ? SizedBox(
+                      child: Center(
+                          child: CircularProgressIndicator(
+                      color: Colors.amber,
+                    )))
+                  : OtpTextField(
+                      textStyle: TextStyle(color: Colors.black),
+                      numberOfFields: 6,
+                      borderColor: Colors.amber,
+                      //set to true to show as box or false to show as dash
+                      showFieldAsBox: true,
+                      //runs when a code is typed in
+                      cursorColor: Colors.amber,
+                      enabledBorderColor: Colors.amber,
+                      focusedBorderColor: Colors.amber,
 
-                    //runs when every textfield is filled
-                    onSubmit: ((value) async {
-                      FirebaseAuth.instance
-                          .signInWithCredential(
-                        PhoneAuthProvider.credential(
-                            verificationId: verificationCode.value,
-                            smsCode: value),
-                      )
-                          .catchError((err) {
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                backgroundColor: Colors.white,
-                                content: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text(
-                                      'OTP is incorrect!',
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 15),
-                                    ),
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    ElevatedButton(
-                                      onPressed: () =>
-                                          Navigator.of(context).pop(),
-                                      style: ButtonStyle(
-                                        shadowColor: MaterialStatePropertyAll(
-                                            Colors.amber),
-                                        elevation: MaterialStatePropertyAll(8),
-                                        shape: MaterialStatePropertyAll(
-                                          RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(26),
-                                          ),
-                                        ),
-                                        backgroundColor:
-                                            MaterialStatePropertyAll(
-                                                Colors.amber),
+                      //runs when every textfield is filled
+                      onSubmit: ((value) async {
+                        FirebaseAuth.instance
+                            .signInWithCredential(
+                          PhoneAuthProvider.credential(
+                              verificationId: verificationCode.value,
+                              smsCode: value),
+                        )
+                            .catchError((err) {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  backgroundColor: Colors.white,
+                                  content: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(
+                                        'OTP is incorrect!',
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 15),
                                       ),
-                                      child: Text('Ok!'),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            });
-                      }).then((value) {
-                        Navigator.of(context).pop();
-                      });
-                    }),
-                  ),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () =>
+                                            Navigator.of(context).pop(),
+                                        style: ButtonStyle(
+                                          shadowColor: MaterialStatePropertyAll(
+                                              Colors.amber),
+                                          elevation:
+                                              MaterialStatePropertyAll(8),
+                                          shape: MaterialStatePropertyAll(
+                                            RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(26),
+                                            ),
+                                          ),
+                                          backgroundColor:
+                                              MaterialStatePropertyAll(
+                                                  Colors.amber),
+                                        ),
+                                        child: Text('Ok!'),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              });
+                        }).then((value) {
+                          Navigator.of(context).pop();
+                        });
+                      }),
+                    );
+            }),
             SizedBox(height: 25),
           ],
         ),
