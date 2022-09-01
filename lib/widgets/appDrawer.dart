698434b5b777dacfe3611b177614a05b0071da7c
Raj Sengupta <rajsengupta@Rohans-MacBook-Pro.local>
2022-09-01 12:25:14 +0530

    developer social media links

--- lib/widgets/appDrawer.dart
+++ lib/widgets/appDrawer.dart
@@ -1,9 +1,13 @@
+import 'package:android_intent_plus/android_intent.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:series/api/seriesdata.dart';
 import 'package:series/screens/favouritesScreen.dart';
 import 'package:series/screens/profileEditScreen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:device_apps/device_apps.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AppDrawer extends StatefulWidget {
   @override
@@ -13,6 +17,10 @@ class AppDrawer extends StatefulWidget {
 class _AppDrawerState extends State<AppDrawer> {
   final SeriesData controller = Get.find();
   static int firstFetch = 1;
+  final String developerInstaUrl = 'https://www.instagram.com/rohaaansen/';
+  final String developerTwitterUrl = 'https://twitter.com/rohan_sen132';
+  final String developerLinkedInUrl =
+      'https://www.linkedin.com/in/rohan-sengupta-193bb916a/';
 
   getUserData() {
     if (firstFetch > 0) {
@@ -29,6 +37,28 @@ class _AppDrawerState extends State<AppDrawer> {
   void initState() {
     getUserData();
     super.initState();
+  }
+
+  Future<void> _launchUrl(String uri, String appPackageName) async {
+    final Uri url = Uri.parse(uri);
+
+    bool isInstalled = await DeviceApps.isAppInstalled(appPackageName);
+
+    if (isInstalled) {
+      AndroidIntent intent = AndroidIntent(
+        action: 'action_view',
+        data: url as String?,
+      );
+      await intent.launch();
+    } else {
+      if (!await launchUrl(
+        url,
+        mode: LaunchMode.externalApplication,
+        // to open in youtube
+      )) {
+        throw 'Could not launch $url';
+      }
+    }
   }
 
   @override
@@ -156,6 +186,42 @@ class _AppDrawerState extends State<AppDrawer> {
                 ),
               ),
             ),
+            Container(
+              margin: EdgeInsets.only(
+                left: 16,
+                bottom: 8,
+              ),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  'Created By Rohan Sengupta',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () =>
+                        _launchUrl(developerInstaUrl, 'com.instagram.android'),
+                    icon: FaIcon(FontAwesomeIcons.instagram),
+                  ),
+                  IconButton(
+                    onPressed: () =>
+                        _launchUrl(developerTwitterUrl, 'com.twitter.android'),
+                    icon: FaIcon(FontAwesomeIcons.twitter),
+                  ),
+                  IconButton(
+                    onPressed: () => _launchUrl(
+                        developerLinkedInUrl, 'com.linkedin.android'),
+                    icon: FaIcon(FontAwesomeIcons.linkedin),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
